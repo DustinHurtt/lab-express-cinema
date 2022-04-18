1af9c76c0e7fbbe42c1826106dae8b55116561fc
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-04-18 16:15:24 -0400

    lab done

--- app.js
+++ app.js
@@ -22,13 +22,68 @@ require('./config')(app);
 const projectName = 'lab-express-cinema';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
+const Movie = require('./models/Movie.models')
+
+const mongoose = require ('mongoose') ;
+
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
 
+app.get('/movies', function(req, res, next) {
+    Movie.find()
+    .then(function(movies){
+      res.render('movies', {movies: movies})
+    })
+    .catch(function (error) {
+      console.log(error);
+  
+    } );
+  });
+
+  app.get('/movies/:details', function(req, res, next) {
+    Movie.findById(req.params.details)
+    .then (function (foundMovie) {
+    console.log(foundMovie)
+      res.render('movie-details', {foundMovie: foundMovie});
+  })
+    .catch(function (error) {
+      console.log(error);
+    });
+  })
+  
+  
+
+// app.post("/movies", function (req, res, next) {
+  
+//     Movie.create({
+//       title: req.body.title,
+//       director: req.body.director,
+//       stars: req.body.stars,
+//       image: req.body.image,
+//       description: req.body.description,
+//       showtimes: req.body.showtimes,
+//     })
+//       .then(function (movies) {
+//         res.redirect("/movies");
+//       })
+//       .catch(function (error) {
+//         res.redirect("/movies");
+//       });
+  
+//     // res.render("index", { title: "Express" });
+//   });
+
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
+
+mongoose
+  .connect('mongodb://localhost/lab-express-cinema') 
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
 
 module.exports = app;
